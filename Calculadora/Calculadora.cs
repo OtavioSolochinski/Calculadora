c3909775eeb3267721ee67bc4b0fb908e9535d78
Severino Gilmar Tavares da Silva <severinogilmartavares@gmail.com>
2025-08-18 19:58:26 -0300

    Criação do método Divisão.

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -8,5 +8,20 @@ namespace Calculadora
 {
     internal class Calculadora
     {
+
+
+        public static decimal Divisao(decimal numero1, decimal numero2)
+        {
+            if (numero1 == 0 || numero2 ==0)
+            {
+                throw new DivideByZeroException("Impossível dividir por zero.");
+            }
+            else
+            {
+                return numero1 / numero2;
+            }
+
+
+        }
     }
 }
